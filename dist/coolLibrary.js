6d84c50ffcf4bea43e187405aad6ed2d0d573d94
Amit Kumar Jha <Amitjha1412@gmail.com>
2015-06-11 21:36:51 +0530

    committing

--- dist/coolLibrary.js
+++ dist/coolLibrary.js
@@ -1,10 +1,10 @@
 (function() {
 	'use strict';
-	var n = document.createElement('div');
-	n.style.backgroundColor = 'teal';
-	n.style.width = '250px';
-	n.style.height = '250px';
-	n.setAttribute('foo', 'bar');
-	n.classList.add('box');
-	document.body.appendChild(n);
+	var a = document.createElement('div');
+	a.style.backgroundColor = 'teal';
+	a.style.width = '250px';
+	a.style.height = '250px';
+	a.setAttribute('foo', 'bar');
+	a.classList.add('box');
+	document.body.appendChild(a);
 })();
